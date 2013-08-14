e8e2cff92831b4d15a2257703965292fab82ddd2
Alexander <iam.asm89@gmail.com>
2013-08-14 14:09:07 +0200

    Add Cors to api. Configured for local development for now.

--- web/index.php
+++ web/index.php
@@ -2,5 +2,20 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Asm89\Stack\Cors;
+use Symfony\Component\HttpFoundation\Request;
+
 $app = require_once __DIR__ . '/../app/bootstrap.php';
-$app->run();
+
+$stackedApp = new Cors($app, array(
+    'allowedOrigins' => array('http://localhost:8000'),
+    'allowedMethods' => array('DELETE', 'GET', 'PUT', 'POST'),
+    'allowedHeaders' => array('accept', 'content-type', 'origin', 'x-requested-with'),
+));
+
+$request = Request::createFromGlobals();
+
+$response = $stackedApp->handle($request);
+$response->send();
+
+$app->terminate($request, $response);
